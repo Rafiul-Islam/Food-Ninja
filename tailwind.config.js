6111d2fc1dacbb53fe9a234978302725a5f99440
Rafiul Islam <rafi.cse.ru@gmail.com>
2021-01-25 11:36:02 +0600

    basic UI complete

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      fontFamily: {
+        nunito: ["Nunito"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
